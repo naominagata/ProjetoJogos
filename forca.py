5fa858bb4cee6c1cb6f953b9eb5f97d77da32319
naominagata <84246266+naominagata@users.noreply.github.com>
2023-04-26 13:34:57 -0300

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,8 @@
+
+def jogar():
+    print("JOGO DE FORCA")
+
+    print("FIM DO JOGO")
+
+if(__name__ == "__main__"):
+    jogar()
---
+++ jogos.py
@@ -0,0 +1,14 @@
+import forca
+import adivinhacao
+
+# forca.py
+
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
