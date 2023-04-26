846ce79ec7231d0115c0bc08f83b1bece63a7ce1
naominagata <84246266+naominagata@users.noreply.github.com>
2023-04-26 15:52:26 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -2,6 +2,18 @@
 def jogar():
     print("JOGO DE FORCA")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while ( not enforcou and not acertou):
+        print("jogando")
+        chute = input("Digite uma letra ")
+        if ( palavra_secreta.find(chute) ):
+            print("")
+        else:
+            print("")
+
     print("FIM DO JOGO")
 
 if(__name__ == "__main__"):
